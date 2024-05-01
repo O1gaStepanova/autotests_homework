ffe6961d4b96be0333d6dde87d0bb26369b25aa9
oa.stepanova <oa.stepanova@tensor.ru>
2024-05-01 12:22:45 +0300

    Решение task_3

--- lection 10/task_3.py
+++ lection 10/task_3.py
@@ -2,6 +2,7 @@
 # Промаркируйте 1 параметр из выборки как smokе, а 1 набор данных скипните
 
 import pytest
+from contextlib import nullcontext as does_not_raise
 
 
 def all_division(*arg1):
@@ -12,5 +13,12 @@ def all_division(*arg1):
     return division
 
 
-
-
+@pytest.mark.parametrize("nom, denom, result, expectation",
+                         [pytest.param(10, 2, 5, does_not_raise(), marks=pytest.mark.smoke),
+                          pytest.param(10, 25, 0.4, does_not_raise(), marks=pytest.mark.skip),
+                          (10, -7, -10 / 7, does_not_raise()),
+                          (10, '0', 1, pytest.raises(TypeError)),
+                          (10, 0, 1, pytest.raises(ZeroDivisionError))])
+def test_all_division(nom, denom, result, expectation):
+    with expectation:
+        assert all_division(nom, denom) == result
