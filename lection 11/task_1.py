293dec43283bf282089df97eb244294a760d6714
oa.stepanova <oa.stepanova@tensor.ru>
2024-05-29 16:50:19 +0300

    Решение task_1

--- lection 11/task_1.py
+++ lection 11/task_1.py
@@ -8,4 +8,42 @@
 
 
 from selenium import webdriver
+from selenium.webdriver import Keys, ActionChains
 from selenium.webdriver.common.by import By
+import time
+
+sbis_site = "https://sbis.ru/"
+driver = webdriver.Chrome()
+
+try:
+    print('Перейти на https://sbis.ru/')
+    driver.get(sbis_site)
+    time.sleep(2)
+
+    print('Перейти в раздел "Контакты"')
+    contacts = driver.find_element(By.CSS_SELECTOR, '.sbisru-Header__menu-item-1')
+    contacts.click()
+    time.sleep(2)
+
+    print('Найти баннер "Тензор", кликнуть по нему')
+    banner = driver.find_element(By.CSS_SELECTOR, '[class="sbisru-Contacts__logo-tensor mb-12"]')
+    banner.click()
+    time.sleep(1)
+
+    print('Перейти на https://tensor.ru/')
+    driver.switch_to.window(driver.window_handles[1])
+
+    print('Проверить, что есть блок новости "Сила в людях"')
+    news_block = driver.find_element(By.CSS_SELECTOR, '.tensor_ru-Index__block4-content')
+    assert news_block.is_displayed(), 'Блок новости "Сила в людях" отсутствует'
+
+    print('Перейти в этом блоке в "Подробнее" и убедиться, что открывается https://tensor.ru/about')
+    about_button = driver.find_element(By.CSS_SELECTOR, '.tensor_ru-link.tensor_ru-Index__link[href="/about"]')
+    action_chains = ActionChains(driver)
+    about_button.send_keys("\n")
+    time.sleep(2)
+    assert driver.current_url == 'https://tensor.ru/about', "Неверный адрес сайта"
+finally:
+    driver.quit()
+
+
